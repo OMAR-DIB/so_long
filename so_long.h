6545175e9a2af7fcb287eed004cf0f9b8b2dc350
Omar Dib <omardib759@gmail.com>
2024-07-20 10:45:07 +1400

    second commit

---
+++ so_long.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: odib <odib@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 18:43:01 by odib              #+#    #+#             */
+/*   Updated: 2024/07/18 18:43:02 by odib             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h> 
+# include <string.h>
+# include <sys/wait.h>
+# include <unistd.h>
+#endif
